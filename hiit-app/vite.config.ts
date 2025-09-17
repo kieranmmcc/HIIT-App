2345423add1a7d5b12e296ad05cf898f89541687
Kieran McCormick <kmccormick@servicetitan.com>
2025-09-17 16:12:56 -0700

    image fixes

--- hiit-app/vite.config.ts
+++ hiit-app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HIIT-App/', // Set base path for GitHub Pages
+  base: '/', // Set base path for custom domain
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
